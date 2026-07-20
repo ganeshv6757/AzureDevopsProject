ec0a966b086842e2ee339e0a41196a36f47f07a5
Ganesh <ganeshv6757@gmail.com>
2026-07-20 22:32:39 +0530

    update text from code

--- AzureWebApp/Controllers/WeatherForecastController.cs
+++ AzureWebApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AzureWebApp.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Ganesh", "THanvi", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
